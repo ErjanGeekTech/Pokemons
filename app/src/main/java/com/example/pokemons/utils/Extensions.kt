fcce3d20fe1730e5de191ac518cc651a224569d6
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-10-01 01:39:49 +0300

    update

--- app/src/main/java/com/example/pokemons/utils/Extensions.kt
+++ app/src/main/java/com/example/pokemons/utils/Extensions.kt
@@ -1,11 +1,8 @@
 package com.example.pokemons.utils
 
-import androidx.fragment.app.Fragment
-
-fun Fragment.isCapitalizing(type: String) =
+fun isCapitalizing(type: String) =
     type.replaceFirstChar {
         if (it.isLowerCase()) it.titlecase(
             java.util.Locale.getDefault()
         ) else it.toString()
-
     }
