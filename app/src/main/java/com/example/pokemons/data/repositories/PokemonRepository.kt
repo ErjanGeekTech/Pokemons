e05fdf039b117fccb31d6b460a7787986700be16
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-10-09 02:31:25 +0600

    delete annotation Inject

--- app/src/main/java/com/example/pokemons/data/repositories/PokemonRepository.kt
+++ app/src/main/java/com/example/pokemons/data/repositories/PokemonRepository.kt
@@ -1,9 +1,8 @@
 package com.example.pokemons.data.repositories
 
 import com.example.pokemons.data.network.apiservices.PokemonApiService
-import javax.inject.Inject
 
-class PokemonRepository @Inject constructor(private val service: PokemonApiService) {
+class PokemonRepository constructor(private val service: PokemonApiService) {
 
     suspend fun fetchPokemon() = service.fetchPokemon()
     suspend fun fetchIdPokemon(id: Int) = service.fetchIdPokemon(id)
