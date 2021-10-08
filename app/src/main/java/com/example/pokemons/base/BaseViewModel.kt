23c0f71b7a46963d3022d9adb3b83a30927e0fec
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-10-09 02:25:17 +0600

    delete uiScope

--- app/src/main/java/com/example/pokemons/base/BaseViewModel.kt
+++ app/src/main/java/com/example/pokemons/base/BaseViewModel.kt
@@ -5,7 +5,4 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 
-open class BaseViewModel : ViewModel() {
-    private val job = Job()
-    val uiScope = CoroutineScope(Dispatchers.Main.plus(job))
-}
+open class BaseViewModel : ViewModel()
