be8e2d13bf91ac89e21bfc76d9ae4e1d12a81c22
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-10-09 02:24:34 +0600

    add koin and startKoin

--- app/src/main/java/com/example/pokemons/App.kt
+++ app/src/main/java/com/example/pokemons/App.kt
@@ -1,7 +1,25 @@
 package com.example.pokemons
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import com.example.pokemons.modules.networkModule
+import com.example.pokemons.modules.repositoryModule
+import com.example.pokemons.modules.vmModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
-@HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            // Koin Android logger
+            androidLogger()
+            //inject Android context
+            androidContext(this@App)
+            // use modules
+            modules(listOf(networkModule, repositoryModule, vmModule))
+        }
+    }
+
+}
